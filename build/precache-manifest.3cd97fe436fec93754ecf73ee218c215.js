4487a64f81d3ed78f934e9748666cc197dc9f526
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2020-07-16 20:49:03 -0400

    BS

---
+++ build/precache-manifest.3cd97fe436fec93754ecf73ee218c215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e13ede48bac88ff719110bfdf1c1deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a634163c1e789a58151",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9a1cd8de00bd889db160",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6a634163c1e789a58151",
+    "url": "/static/js/2.527ba260.chunk.js"
+  },
+  {
+    "revision": "0cb41c36e136d1fa8dc517f20703f9ee",
+    "url": "/static/js/2.527ba260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1cd8de00bd889db160",
+    "url": "/static/js/main.9e8274eb.chunk.js"
+  },
+  {
+    "revision": "7b92cb8369cae5c2b71c",
+    "url": "/static/js/runtime-main.38394762.js"
+  }
+]);
